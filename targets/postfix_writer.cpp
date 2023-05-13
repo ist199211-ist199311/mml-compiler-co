5b994bc84ad23d0a8073f3dd6ef7936e2d12a318
Diogo Correia <diogo.t.correia@tecnico.ulisboa.pt>
2023-05-13 19:52:36 +0100

    feat(ast): add `pointer_index_node` (#21)

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -150,6 +150,12 @@ void mml::postfix_writer::do_variable_node(cdk::variable_node * const node, int 
   _pf.ADDR(node->name());
 }
 
+void mml::postfix_writer::do_pointer_index_node(mml::pointer_index_node * const node, int lvl) {
+  ASSERT_SAFE_EXPRESSIONS;
+  // TODO: implement this
+  throw "not implemented";
+}
+
 void mml::postfix_writer::do_rvalue_node(cdk::rvalue_node * const node, int lvl) {
   ASSERT_SAFE_EXPRESSIONS;
   node->lvalue()->accept(this, lvl);
