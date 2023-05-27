341fa6a3ff921254ae11b14ec48f99d57fe3f486
Diogo Correia <diogo.t.correia@tecnico.ulisboa.pt>
2023-05-27 18:42:00 +0100

    feat(parser): add "logic and" and "logic or" binary operators

--- targets/xml_writer.cpp
+++ targets/xml_writer.cpp
@@ -19,10 +19,12 @@ void mml::xml_writer::do_not_node(cdk::not_node * const node, int lvl) {
   do_unary_operation(node, lvl);
 }
 void mml::xml_writer::do_and_node(cdk::and_node * const node, int lvl) {
-  // EMPTY
+  // TODO ASSERT_SAFE_EXPRESSIONS
+  do_binary_operation(node, lvl);
 }
 void mml::xml_writer::do_or_node(cdk::or_node * const node, int lvl) {
-  // EMPTY
+  // TODO ASSERT_SAFE_EXPRESSIONS
+  do_binary_operation(node, lvl);
 }
 
 //---------------------------------------------------------------------------
