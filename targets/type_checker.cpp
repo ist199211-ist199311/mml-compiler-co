436ab166e7d2d1d1c809f20d1aaa45e4c3dc6a77
Rafael Oliveira <rafael.serra.oliveira@tecnico.ulisboa.pt>
2023-06-12 11:33:18 +0100

    feat: type check while instruction (#179)
    Closes #149

--- targets/type_checker.cpp
+++ targets/type_checker.cpp
@@ -431,6 +431,10 @@ void mml::type_checker::do_input_node(mml::input_node *const node, int lvl) {
 
 void mml::type_checker::do_while_node(mml::while_node *const node, int lvl) {
   node->condition()->accept(this, lvl + 4);
+
+  if (!node->condition()->is_typed(cdk::TYPE_INT)) {
+    throw std::string("wrong type in condition of loop instruction");
+  }
 }
 
 //---------------------------------------------------------------------------
