43c42867473933105e6282c482823d9670a7c234
Diogo Correia <diogo.t.correia@tecnico.ulisboa.pt>
2023-06-13 13:47:39 +0100

    feat: emit postfix for addition and subtraction expressions
    Closes #135

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -101,15 +101,57 @@ void mml::postfix_writer::do_not_node(cdk::not_node * const node, int lvl) {
 
 void mml::postfix_writer::do_add_node(cdk::add_node * const node, int lvl) {
   ASSERT_SAFE_EXPRESSIONS;
+
   node->left()->accept(this, lvl);
+  if (node->is_typed(cdk::TYPE_DOUBLE) && !node->left()->is_typed(cdk::TYPE_DOUBLE)) {
+    _pf.I2D();
+  } else if (node->is_typed(cdk::TYPE_POINTER) && node->left()->is_typed(cdk::TYPE_INT)) {
+    auto ref = cdk::reference_type::cast(node->type());
+    _pf.INT(ref->referenced()->size());
+    _pf.MUL();
+  }
+
   node->right()->accept(this, lvl);
-  _pf.ADD();
+  if (node->is_typed(cdk::TYPE_DOUBLE) && !node->right()->is_typed(cdk::TYPE_DOUBLE)) {
+    _pf.I2D();
+  } else if (node->is_typed(cdk::TYPE_POINTER) && node->right()->is_typed(cdk::TYPE_INT)) {
+    auto ref = cdk::reference_type::cast(node->type());
+    _pf.INT(ref->referenced()->size());
+    _pf.MUL();
+  }
+
+  if (node->is_typed(cdk::TYPE_DOUBLE)) {
+    _pf.DADD();
+  } else {
+    _pf.ADD();
+  }
 }
 void mml::postfix_writer::do_sub_node(cdk::sub_node * const node, int lvl) {
   ASSERT_SAFE_EXPRESSIONS;
+
   node->left()->accept(this, lvl);
+  if (node->is_typed(cdk::TYPE_DOUBLE) && !node->left()->is_typed(cdk::TYPE_DOUBLE)) {
+    _pf.I2D();
+  } else if (node->is_typed(cdk::TYPE_POINTER) && node->left()->is_typed(cdk::TYPE_INT)) {
+    auto ref = cdk::reference_type::cast(node->type());
+    _pf.INT(ref->referenced()->size());
+    _pf.MUL();
+  }
+
   node->right()->accept(this, lvl);
-  _pf.SUB();
+  if (node->is_typed(cdk::TYPE_DOUBLE) && !node->right()->is_typed(cdk::TYPE_DOUBLE)) {
+    _pf.I2D();
+  } else if (node->is_typed(cdk::TYPE_POINTER) && node->right()->is_typed(cdk::TYPE_INT)) {
+    auto ref = cdk::reference_type::cast(node->type());
+    _pf.INT(ref->referenced()->size());
+    _pf.MUL();
+  }
+
+  if (node->is_typed(cdk::TYPE_DOUBLE)) {
+    _pf.DSUB();
+  } else {
+    _pf.SUB();
+  }
 }
 
 void mml::postfix_writer::prepareIDBinaryExpression(cdk::binary_operation_node * const node, int lvl) {
