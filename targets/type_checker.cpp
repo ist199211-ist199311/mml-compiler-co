0afe16ca075e2b654f69e1f33d2fc8f93621f3d8
Diogo Correia <diogo.t.correia@tecnico.ulisboa.pt>
2023-06-15 16:28:39 +0100

    fix: apply changes to while node

--- targets/type_checker.cpp
+++ targets/type_checker.cpp
@@ -496,7 +496,9 @@ void mml::type_checker::do_input_node(mml::input_node *const node, int lvl) {
 void mml::type_checker::do_while_node(mml::while_node *const node, int lvl) {
   node->condition()->accept(this, lvl + 4);
 
-  if (!node->condition()->is_typed(cdk::TYPE_INT)) {
+  if (node->condition()->is_typed(cdk::TYPE_UNSPEC)) {
+    node->condition()->type(cdk::primitive_type::create(4, cdk::TYPE_INT));
+  } else if (!node->condition()->is_typed(cdk::TYPE_INT)) {
     throw std::string("wrong type in condition of loop instruction");
   }
 }
